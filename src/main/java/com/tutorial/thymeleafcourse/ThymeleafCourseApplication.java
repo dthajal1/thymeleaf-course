ec40efd036bea3093c75c76531241413246e641d
Diraj Thajali <diraj.thajali@berkeley.edu>
2020-08-22 13:29:57 -0700

    Initialized project

---
+++ src/main/java/com/tutorial/thymeleafcourse/ThymeleafCourseApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.thymeleafcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafCourseApplication.class, args);
+	}
+
+}
