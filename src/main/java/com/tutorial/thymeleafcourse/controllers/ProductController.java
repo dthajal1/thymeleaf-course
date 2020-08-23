9aab34112dbaa8d3abe1b4f82c427dab55f9caaf
Diraj Thajali <diraj.thajali@berkeley.edu>
2020-08-22 17:30:00 -0700

    created and used thymeleaf fragment

--- src/main/java/com/tutorial/thymeleafcourse/controllers/ProductController.java
+++ src/main/java/com/tutorial/thymeleafcourse/controllers/ProductController.java
@@ -1,10 +1,12 @@
 package com.tutorial.thymeleafcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProductController {
 
+    @GetMapping("/product")
     public String getProduct() {
         return "product";
     }
