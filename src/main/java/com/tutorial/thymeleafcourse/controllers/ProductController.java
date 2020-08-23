e887a8592b28c0da8570d11326ac8764da338e34
Diraj Thajali <diraj.thajali@berkeley.edu>
2020-08-22 18:18:09 -0700

    refactored to display object's field names using th:text

--- src/main/java/com/tutorial/thymeleafcourse/controllers/ProductController.java
+++ src/main/java/com/tutorial/thymeleafcourse/controllers/ProductController.java
@@ -1,13 +1,31 @@
 package com.tutorial.thymeleafcourse.controllers;
 
+import com.tutorial.thymeleafcourse.services.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ProductController {
 
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/product")
     public String getProduct() {
+        return "redirect:/index";
+    }
+
+    @GetMapping("/product/{id}")
+    public String getProductById(@PathVariable String id, Model model) {
+
+        model.addAttribute("product", productService.getProductById(Integer.valueOf(id)));
         return "product";
     }
+
+
 }
--- src/main/java/com/tutorial/thymeleafcourse/services/ProductService.java
+++ src/main/java/com/tutorial/thymeleafcourse/services/ProductService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface ProductService {
 
-    Product getProduct(Integer id);
+    Product getProductById(Integer id);
 
     List<Product> listProducts();
 }
