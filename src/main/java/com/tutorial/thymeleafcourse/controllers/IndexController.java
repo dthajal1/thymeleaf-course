14295bbd919c4ec1b888b409fb49448f319441cd
Diraj Thajali <diraj.thajali@berkeley.edu>
2020-08-22 19:09:45 -0700

    listed all the courses using thymeleaf iteration

--- src/main/java/com/tutorial/thymeleafcourse/controllers/IndexController.java
+++ src/main/java/com/tutorial/thymeleafcourse/controllers/IndexController.java
@@ -1,13 +1,22 @@
 package com.tutorial.thymeleafcourse.controllers;
 
+import com.tutorial.thymeleafcourse.services.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
+    private final ProductService productService;
+
+    public IndexController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping({"", "/"})
-    public String getIndex() {
+    public String getIndex(Model model) {
+        model.addAttribute("products", productService.listProducts());
         return "index";
     }
 }
